e6b9f3039fd2272b9af2e18dc1b4ebfa4aae5900
Shihang Wang <53704527+Shihang-Wang-58@users.noreply.github.com>
2026-01-18 19:50:50 +0800

    update_PropPredictor.py

--- ouroboros/PropPredictor.py
+++ ouroboros/PropPredictor.py
@@ -11,17 +11,17 @@ if __name__ == "__main__":
     job_name = f'{os.path.splitext(os.path.basename(sys.argv[3]))[0]}_{os.path.basename(sys.argv[1])}'
     extrnal_data = prepare(extrnal_data, smiles_column = smiles_col, standardize = False)
     print('NOTE: loading models...')
-    for predictor in [ p for p in os.listdir(model_name) if os.path.exists(f'{model_name}/{p}/Decoder.pt')]:
+    for predictor_name_str in [ p for p in os.listdir(model_name) if os.path.exists(f'{model_name}/{p}/Decoder.pt')]:
         ## read the predictor models
         predictor = Ouroboros(
             model_name = model_name, 
             batch_size = 512,
-            predictor_info = {predictor: 1.0},
+            predictor_info = {predictor_name_str: 1.0},
             generator = False
         )
         ## prepare
         print('NOTE: make predictions...')
-        predicted_res = predictor.predict_scores(extrnal_data, smiles_column = smiles_col)
+        extrnal_data = predictor.predict_scores(extrnal_data, smiles_column = smiles_col)
     ## output the results
-    predicted_res.to_csv(f"{job_name}_predictions.csv", index=False, header=True, sep=',')
+    extrnal_data.to_csv(f"{job_name}_predictions.csv", index=False, header=True, sep=',')
     print(f'NOTE: job completed! check {job_name}_predictions.csv for results!')
